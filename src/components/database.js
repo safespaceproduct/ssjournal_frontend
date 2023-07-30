ebbefdc47327b749a86d3b2f3f961b237ebff55c
Nicholas Ang <ang.tze.yong@gmail.com>
2023-07-30 16:31:06 +0800

    Add token based routing

--- src/components/database.js
+++ src/components/database.js
@@ -20,7 +20,7 @@ export function getEntries() {
 }
 
 export async function postEntry(entry, user_id) {
-  const response = await fetch(`${API_URL}/journalentry/`, {
+  const response = await fetch(`${API_URL}/journalentry/?token=${user_id}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -40,7 +40,7 @@ export async function postEntry(entry, user_id) {
 }
 
 export async function fetchEntry(user_id) {
-  const response = await fetch(`${API_URL}/journalentry?user=${user_id}`);
+  const response = await fetch(`${API_URL}/journalentry/?token=${user_id}`);
   if (!response.ok) {
     throw new Error(`Failed to fetch entry: ${response.status}`);
   }
