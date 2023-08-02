5f1311cd4e2599aa295ddb603e600ebd27b51a0a
Nicholas Ang <ang.tze.yong@gmail.com>
2023-08-02 22:57:10 +0800

    Change url

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 const config = {
 //   API_URL: "http://localhost:8000",
   // API_URL: "https://ssjournal-backend.onrender.com",
-  API_URL: "http://ec2-13-214-132-61.ap-southeast-1.compute.amazonaws.com",
+  API_URL: "https://ec2-13-214-132-61.ap-southeast-1.compute.amazonaws.com",
 };
 
 export default config;
