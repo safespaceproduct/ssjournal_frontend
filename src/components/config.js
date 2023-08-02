2896977cb221778414cbf0de63884cf2fddcc554
Nicholas Ang <ang.tze.yong@gmail.com>
2023-08-03 01:06:25 +0800

    Add url

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 const config = {
 //   API_URL: "http://localhost:8000",
   // API_URL: "https://ssjournal-backend.onrender.com",
-  API_URL: "https://ec2-52-221-55-20.ap-southeast-1.compute.amazonaws.com",
+  API_URL: "https://api.safespacejournal.link",
 };
 
 export default config;
