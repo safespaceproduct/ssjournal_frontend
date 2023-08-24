2f3123f916475b52ef1375c1dd99b2aa4b454044
Nicholas Ang <ang.tze.yong@gmail.com>
2023-08-24 11:28:22 +0800

    Commit

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import JournalEntry from "./components/JournalEntry";
 import "./App.css";
 
-// function App() {
+// function App(){
 //   return (
-//     <HashRouter>
-//       <div className="App">
-//         <Routes>
-//           <Route
-//             path="users/:user_id"
-//             element={<JournalEntry showBox={true}/>}
-//           />
-//         </Routes>
-//       </div>
-//     </HashRouter>
-//   );
+//     <div>Under maintenance!</div>
+//   )
 // }
 
-function App(){
+function App() {
   return (
-    <div>Under maintenance!</div>
-  )
+    <HashRouter>
+      <div className="App">
+        <Routes>
+          <Route
+            path="users/:user_id"
+            element={<JournalEntry showBox={true}/>}
+          />
+        </Routes>
+      </div>
+    </HashRouter>
+  );
 }
+
+
 
 export default App;
