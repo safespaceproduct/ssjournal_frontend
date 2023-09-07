3808c7d85672a0d37d5bc3dd79710acf715ac8ad
Nicholas Ang <ang.tze.yong@gmail.com>
2023-09-07 16:16:45 +0800

    Edit config

--- src/components/config.js
+++ src/components/config.js
@@ -1,7 +1,7 @@
 const config = {
-  API_URL: "http://localhost:8000",
+  // API_URL: "http://localhost:8000",
   // API_URL: "https://ssjournal-backend.onrender.com",
-  // API_URL: "https://api.safespacejournal.link",
+  API_URL: "https://api.safespacejournal.link",
 };
 
 export default config;
